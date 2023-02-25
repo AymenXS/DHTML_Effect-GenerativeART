bf14760a6448f49dc38ebb52b1ce84887aa458fa
AymenXS <aymenghaloua@gmail.com>
2023-02-25 23:24:24 +0100

    Adding: Animated Lines Variation

--- main.js
+++ main.js
@@ -6,29 +6,48 @@ canvas.height = window.innerHeight;
 class Line {
   constructor(canvas) {
     this.canvas = canvas;
-    this.startX = Math.random() * this.canvas.width;
-    this.startY = Math.random() * this.canvas.height;
-    this.endX = Math.random() * this.canvas.width;
-    this.endY = Math.random() * this.canvas.height;
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
     this.lineWidth = Math.floor(Math.random() * 15 + 1);
     this.hue = Math.floor(Math.random() * 360);
+    this.maxLength = 5;
   }
   draw(context) {
-    context.strokeStyle = "hsl(" + this.hue + ",100%, 50%)";
+    context.strokeStyle = "hsl(" + this.hue + ", 100%, 50%)";
     context.lineWidth = this.lineWidth;
     context.beginPath();
-    context.moveTo(this.startX, this.startY);
-    context.lineTo(this.endX, this.endY);
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
+    }
     context.stroke();
+  }
+  update() {
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history.push({ x: this.x, y: this.y });
+    if (this.history.length > this.maxLength){
+      this.history.shift();
+    }
   }
 }
 
 const lineArrays = [];
-const linesAmount = 30;
+const linesAmount = 2;
 const line = new Line(canvas);
 
 for (i = 0; i < linesAmount; i++) {
   lineArrays[i] = new Line(canvas);
 }
-console.log(lineArrays);
 lineArrays.forEach((line) => line.draw(ctx));
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lineArrays.forEach((line) => {
+    // line.draw(ctx);
+    line.update();
+  });
+  requestAnimationFrame(animate);
+}
+animate();
---
+++ variations/animated-lines/script.js
@@ -0,0 +1,53 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Line {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
+    this.lineWidth = Math.floor(Math.random() * 15 + 1);
+    this.hue = Math.floor(Math.random() * 360);
+    this.maxLength = 5;
+  }
+  draw(context) {
+    context.strokeStyle = "hsl(" + this.hue + ", 100%, 50%)";
+    context.lineWidth = this.lineWidth;
+    context.beginPath();
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
+    }
+    context.stroke();
+  }
+  update() {
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history.push({ x: this.x, y: this.y });
+    if (this.history.length > this.maxLength) {
+      this.history.shift();
+    }
+  }
+}
+
+const lineArrays = [];
+const linesAmount = 2;
+const line = new Line(canvas);
+
+for (i = 0; i < linesAmount; i++) {
+  lineArrays[i] = new Line(canvas);
+}
+lineArrays.forEach((line) => line.draw(ctx));
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lineArrays.forEach((line) => {
+    // line.draw(ctx);
+    line.update();
+  });
+  requestAnimationFrame(animate);
+}
+animate();
