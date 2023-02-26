c3d1e94e02b29696ddb8ebe7bbd091f3641a7f7e
AymenXS <aymenghaloua@gmail.com>
2023-02-26 23:32:05 +0100

    Wrapping Up

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-// Animated Lines
+// Animated Spirals
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// Canvas Shadows
+ctx.shadowOffsetX = 2;
+ctx.shadowOffsetY = 2;
+ctx.shadowColor = "black";
+ctx.shadowBlur = 5;
 
 class Line {
   // Declaration of the variables used to create one Line: Start/End Position, Line Width, Color, Path and MaxLength of the Path
@@ -14,7 +20,14 @@ class Line {
     this.history = [{ x: this.x, y: this.y }];
     this.lineWidth = Math.floor(Math.random() * 15 + 1);
     this.hue = Math.floor(Math.random() * 360);
-    this.maxLength = 5;
+    this.maxLength = Math.floor(Math.random() * 150 + 10);
+    this.speedX = Math.random() * 1 - 0.5;
+    this.speedY = 7;
+    this.lifeSpan = this.maxLength * 2;
+    this.timer = 0;
+    this.angle = 0;
+    this.curve = 0.05;
+    this.velocityCurve = 0.05;
   }
   // The function for drawing one Line
   draw(context) {
@@ -29,18 +42,35 @@ class Line {
   }
   // The function for updating a Line's path
   update() {
-    this.x = Math.random() * this.canvas.width;
-    this.y = Math.random() * this.canvas.height;
-    this.history.push({ x: this.x, y: this.y });
-    if (this.history.length > this.maxLength) {
+    this.timer++;
+    this.angle += 0.1;
+    this.curve += this.velocityCurve;
+    if (this.timer < this.lifeSpan) {
+      this.x += Math.sin(this.angle) * this.curve;
+      this.y += Math.cos(this.angle) * this.curve;
+      this.history.push({ x: this.x, y: this.y });
+      if (this.history.length > this.maxLength) {
+        this.history.shift();
+      }
+    } else if (this.history.length <= 1) {
+      this.reset();
+    } else {
       this.history.shift();
     }
+  }
+  reset() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
+    this.timer = 0;
+    this.angle = 0;
+    this.curve = 0;
   }
 }
 
 // Initiation of the class, the amount of lines to be draw and the execution
 const lineArrays = [];
-const linesAmount = 3;
+const linesAmount = 30;
 const line = new Line(canvas);
 
 for (i = 0; i < linesAmount; i++) {
